db0fcc68a351044f3de32ee047831d300b794930
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-20 08:19:05 +0200

    Get bookname & author name from the user

--- src/book.rs
+++ src/book.rs
@@ -6,18 +6,26 @@ pub struct Book;
 impl Book {
     pub fn new(name: &str, force: bool) {
         let path = Path::new(name);
-
-        if path.exists() {
-            panic!("'{}' already exist", name)
+        if path.exists() && force == false {
+            panic!("`{}` already exist", name)
+        } else if path.exists() && force == true {
+            fs::remove_dir_all(path).unwrap();
         }
 
         fs::create_dir_all(format!("./{name}/src")).unwrap();
 
-        let book_config = include_str!("../assets/book.json");
-
-        let book_name = Text::new("Book name")
+        let mut book_name = Text::new("Choose a book name")
+            .with_help_message("Will be displayed at the book cover")
+            .with_default(name)
+            .with_placeholder(name)
             .prompt()
-            .expect("Failed to read user input");
+            .expect("Failed to get user input");
+
+        let mut author = Text::new("Author name")
+            .prompt()
+            .expect("Failed to get user input");
+
+        let book_config = include_str!("../assets/book.json");
 
         fs::write(format!("./{}/book.json", name), book_config).unwrap();
     }
