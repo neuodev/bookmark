79b10bf09691db2e2b94ad9718665d2b5a04405b
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-20 11:28:48 +0200

    Add style.css

--- src/node.rs
+++ src/node.rs
@@ -1,4 +1,4 @@
-use crate::tokens::{CodeBlock, Heading, LineBreak, List, Paragraph, Quote};
+use crate::tokens::{CodeBlock, Heading, List, Paragraph, Quote};
 
 /// Awrapper enum for all the supported MD blocks
 #[derive(Debug)]
